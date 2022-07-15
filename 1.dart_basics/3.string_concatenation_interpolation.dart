749a8419931230b841dfb4cc19ecc5835993e5a7
udayanudai3421 <udayanudai3421@gmail.com>
2022-07-16 00:11:36 +0530

    Add string concatenation , escaping , etc programs

---
+++ 1.dart_basics/3.string_concatenation_interpolation.dart
@@ -0,0 +1,11 @@
+void main() {
+  String firstName = 'Udayan';
+  String lastName = 'Udai';
+  int age = 18;
+  double height = 6.34;
+
+  print("My Name is " +
+      firstName +
+      " $lastName. I'm $age years old ,I'm $height meters tall.");
+  print('Next year I will be ${age + 1} years old');
+}
---
+++ 1.dart_basics/4.string_escaping.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("Today I'm feeling great");
+  print('Today I\'m feeling great');
+  print('10Rs\\-');
+  print('\$500');
+  print(r'C:\Windows\system32');
+}
